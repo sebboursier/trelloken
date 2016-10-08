28c21df9a50aa1d5f8a77669e96ab9a2b945a800
sebboursier <seb.boursier.33@gmail.com>
2016-10-08 15:31:33 +0200

    Listing OK, Add Board OK

--- index.js
+++ index.js
@@ -14,15 +14,28 @@ commander.version('1.0.0')
   .option('-g, --getToken', 'Get the token user by your default browser')
   .option('-s, --setToken [token]', 'Set the user token')
   .option('-o, --offline', 'To post data offline')
-  .option('-s, --syncro', 'To syncro datas set offline on Trello')
-  .option('-l, --list', 'Action of listing')
+  .option('-p, --push', 'To syncro datas set offline on Trello')
+  .option('-L, --listing', 'Action of listing')
+  .option('-A, --adding', 'Action of adding')
   .option('-b, --board [board]', 'Action on a board')
+  .option('-l, --list [column]', 'Action on a list')
+  .option('-c, --card', 'Action on a card')
   .on('--help', function(){
     console.log('  Examples:')
     console.log('')
-    console.log('    $ trelloken -lb')
+    console.log('    $ trelloken -Lb')
     console.log('        This will list all your boards')
+    console.log('')
+    console.log('    $ trelloken -b BOARD_NAME -Ll')
+    console.log('        This will list all your lists of your existing board BOARD_NAME')
     console.log('')
+    console.log('    $ trelloken -b BOARD_NAME -l LIST_NAME -Lc')
+    console.log('        This will list all your card of your existing list LIST_NAME of your existing board BOARD_NAME')
+    console.log('')
+    console.log('  Notes:')
+    console.log('')
+    console.log('    If your parameter contain space, write it between quote : "some board"')
+    console.log('')
   })
 
 commander.parse(process.argv)
@@ -40,7 +53,7 @@ if(commander.getToken) {
     }
   ]).then((answers) => {
     if(answers.confirm) {
-      open(`https://trello.com/1/connect?key=${apiKey}&name=trelloken&response_type=token`)
+      open(`https://trello.com/1/connect?key=${apiKey}&name=trelloken&response_type=token&scope=read,write`)
     } else {
       console.log("Okay, but remember you can't use TrelLoken as this isn't done.")
     }
@@ -55,14 +68,134 @@ if(commander.getToken) {
     console.log("OFFLINE")
 } else {
     fs.readFile(`${__dirname}\\token`, 'utf8', (err, data) => {
-      var trello = new node_trello(apiKey, data)
-      online(trello)
+      if(typeof data == 'undefined') {
+        console.log("You don't set your token!")
+        console.log('Please run "trelloken -g"')
+      } else {
+        var trello = new node_trello(apiKey, data)
+        online(trello)
+      }
     })
 }
 
-function online(trello) {
+// OPTIONS FUNCTION
+
+function online (trello) {
+  if (commander.listing && commander.adding) {
+    console.log(`You can't use both "-L" and "-A" options`);
+  } else if (commander.listing) {
+    onlineListing(trello)
+  } else if (commander.adding) {
+    onlineAdding(trello)
+  }
+}
+
+function onlineAdding (trello) {
+
   if (commander.board && commander.list) {
+    console.log("-- Command : Add a list")
+    // TODO
+  } else if (commander.board) {
+
+    console.log("-- Command : Add a board")
+
+    if(typeof commander.board == 'boolean') {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Board Name?"
+        }
+      ]).then((answers) => {
+        onlineAddBoard(trello,answers.name)
+      })
+    } else {
+      onlineAddBoard(trello,commander.board)
+    }
+
+    function onlineAddBoard (trello, boardName) {
+      trello.post("/1/boards", { name: boardName} , (err,newBoard) => {
+        if (err) throw err
+        console.log(`Success! Your board has been created.`)
+        console.log(`Name : ${newBoard.name}`)
+        console.log(`Id : ${newBoard.id}`)
+        console.log(`Access Url : ${newBoard.url}`)
+      })
+    }
+
+  }
+}
+
+function onlineListing (trello) {
+  if (commander.board && commander.list && commander.card) {
+
+    console.log("-- Command : List Columns of a board")
+
+    trello.get("/1/members/me/boards", (err, boards) => {
+      if (err) throw err
+      let boardFind = false
+      for (board in boards) {
+        if (boards[board].name == commander.board) {
+          let boardId = boards[board].id
+          boardFind = true;
+          trello.get(`/1/boards/${boardId}/lists`, (err, lists) => {
+            if (err) throw err
+            let listFind = false;
+            for (list in lists) {
+              if (lists[list].name == commander.list) {
+                listFind = true;
+                trello.get(`/1/lists/${lists[list].id}/cards`, (err, cards) => {
+                  if (err) throw err
+                  for (card in cards) {
+                    console.log(cards[card].name)
+                  }
+                })
+                break
+              }
+            }
+            if(!listFind) {
+              console.log(`There are no list named as "${commander.list}" in the board "${commander.board}"`)
+              console.log(`Please check "trelloken -b ${commander.board} -Ll" for the available lists`)
+            }
+          })
+          break
+        }
+      }
+      if(!boardFind) {
+        console.log(`There are no board named as "${commander.board}"`)
+        console.log(`Please check "trelloken -Lb" for the available boards`)
+      }
+    })
+
+  } else if (commander.board && commander.list) {
+
+    console.log("-- Command : List Columns of a board")
+
+    trello.get("/1/members/me/boards", (err, data) => {
+      if (err) throw err
+      let find = false;
+      for (board in data) {
+        if (data[board].name == commander.board) {
+          trello.get(`/1/boards/${data[board].id}/lists`, (err, data) => {
+            if (err) throw err
+            for (list in data) {
+              console.log(data[list].name)
+            }
+          })
+          find = true;
+          break
+        }
+      }
+      if(!find) {
+        console.log(`There are no board named as "${commander.board}"`)
+        console.log(`Please check "trelloken -Lb" for the available boards`)
+      }
+    })
+
+  } else if (commander.board) {
+
     console.log("-- Command : List Boards")
+
     trello.get("/1/members/me/boards", (err, data) => {
       if (err) throw err
       for (board in data) {
